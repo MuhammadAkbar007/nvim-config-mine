99729647c384cedf0b57d59371b9543d03dace14
MuhammadAkbar007 <fotismenos007@mail.ru>
2023-07-23 14:15:54 +0500

    Bismillah

---
+++ init.lua
@@ -0,0 +1,9 @@
+require('core.options')
+require('core.packer')
+require('core.keymaps')
+
+require('plugins.nvim-treesitter')
+require('plugins.nvim-telescope')
+require('plugins.nvim-tree')
+require('plugins.nvim-cmp')
+require('plugins.lsp.mason')
---
+++ lua/core/options.lua
@@ -0,0 +1,6 @@
+local opt = vim.opt
+opt.nu = true
+opt.autoindent = true
+opt.shiftwidth = 4
+opt.ignorecase = true
+opt.clipboard:append("unnamedplus")
