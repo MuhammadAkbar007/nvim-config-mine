8d7b6d113069513506a116f8812d6f41465d106b
MuhammadAkbar007 <fotismenos007@mail.ru>
2023-07-26 19:31:53 +0500

    bufferline for tabs

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,92 +1,95 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-  use("windwp/nvim-autopairs")
-  use("windwp/nvim-ts-autotag")
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
 
-  use 'nvim-tree/nvim-web-devicons'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
-	  'nvim-tree/nvim-tree.lua',
-	  requires = {
-		  'nvim-tree/nvim-web-devicons', -- optional
-	  },
-  }
+	use("nvim-tree/nvim-web-devicons")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	})
 
- use("nvim-lualine/lualine.nvim")
+	use("nvim-lualine/lualine.nvim")
 
-  -- Using Packer:
-  use 'Mofiqul/dracula.nvim'
+	-- Using Packer:
+	use("Mofiqul/dracula.nvim")
 
---  use({
---	  'projekt0n/github-nvim-theme',
---	  config = function()
---		  require('github-theme').setup({
---			  options = {
---				transparent = true,
---				dim_inactive = true, darken = {                 -- Darken floating windows and sidebar-like windows floats = false,
---				sidebars = {
---					enable = true,
---					list = {},             -- Apply dark background to specific windows
---				},
---			},
---			  }
---		  })
---
---		  vim.cmd('colorscheme github_dark_colorblind')
---	  end
---  })
+	--  use({
+	--	  'projekt0n/github-nvim-theme',
+	--	  config = function()
+	--		  require('github-theme').setup({
+	--			  options = {
+	--				transparent = true,
+	--				dim_inactive = true, darken = {                 -- Darken floating windows and sidebar-like windows floats = false,
+	--				sidebars = {
+	--					enable = true,
+	--					list = {},             -- Apply dark background to specific windows
+	--				},
+	--			},
+	--			  }
+	--		  })
+	--
+	--		  vim.cmd('colorscheme github_dark_colorblind')
+	--	  end
+	--  })
 
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
-  use("L3MON4D3/LuaSnip")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("L3MON4D3/LuaSnip")
 
-  use("saadparwaiz1/cmp_luasnip")
-  use("rafamadriz/friendly-snippets")
+	use("saadparwaiz1/cmp_luasnip")
+	use("rafamadriz/friendly-snippets")
 
-  use("williamboman/mason.nvim")
-  use("williamboman/mason-lspconfig.nvim")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
 
-  use("neovim/nvim-lspconfig")
-  use("hrsh7th/cmp-nvim-lsp")
-  use({
-	  "glepnir/lspsaga.nvim",
-	  branch = "main",
-	  requires = {
-		  { "nvim-tree/nvim-web-devicons" },
-		  { "nvim-treesitter/nvim-treesitter" },
-	  },
-  }) -- enhanced lsp uis
-  use("mfussenegger/nvim-jdtls")
-  use("onsails/lspkind.nvim")
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use({
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+		requires = {
+			{ "nvim-tree/nvim-web-devicons" },
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+	}) -- enhanced lsp uis
+	use("mfussenegger/nvim-jdtls")
+	use("onsails/lspkind.nvim")
 
-  use("jose-elias-alvarez/null-ls.nvim")
-  use("jayp0521/mason-null-ls.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
 
-  use("lewis6991/gitsigns.nvim")
+--	use("lewis6991/gitsigns.nvim")
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	use({ "akinsho/bufferline.nvim", tag = "*", requires = "nvim-tree/nvim-web-devicons" })
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
